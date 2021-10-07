e686d7557616b70fc0dae13f9125da0f3b199d20
jackson <247955056@qq.com>
2021-10-07 13:33:07 +0800

    init

---
+++ src/code-area/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <textarea ref="codearea"></textarea>
+</template>
+
+<script>
+import Codemirror from 'codemirror'
+export default {
+  mounted() {}
+}
+</script>
+
+<style>
+</style>
